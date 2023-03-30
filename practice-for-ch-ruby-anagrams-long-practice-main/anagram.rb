561c77415f24e09ac959b79b3cedb9aef61aebc3
Jiamin-Zou <jiaminz622@gmail.com>
2023-03-30 13:28:30 -0400

    Completed anagram

--- practice-for-ch-ruby-anagrams-long-practice-main/anagram.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagram.rb
@@ -1,4 +1,5 @@
 def anagram?(str1, str2) #O(n!)
+    return false if str1.length != str2.length
     arr = str1.chars.permutation.to_a #factorial
     arr.include?(str2.chars) #linear
 end
@@ -14,6 +15,25 @@ def sec_anagram?(str1, str2) #O(n^2)
     str_arr.empty? #linear
 end
 
-def 
-p sec_anagram?("gizmo", "sally")
-p sec_anagram?("elvis", "lives")
+def third_anagram?(str1, str2)
+    return false if str1.length != str2.length
+    str1.chars.sort == str2.chars.sort
+    # chars is linear O(n)
+    # Array.sort is loglinear O(n log n)
+    # array == array is linear O(n)
+    #O (n+ n log n + n)
+    # O(n log n)
+end
+
+def fourth_anagram?(str1, str2) #O(n)
+    return false if str1.length != str2.length
+    counter = Hash.new(0) # constant
+    str1.each_char {|char| counter[char] += 1} # linear (constant) = linear
+    str2.each_char {|char| counter[char] -= 1} # linear (constant) = linear
+    counter.values.all?{|val| val.zero?} # linear + linear (constant)
+    # O(4n)
+    # O(n)
+
+end
+p fourth_anagram?("gizmo", "sally")
+p fourth_anagram?("elvis", "lives")
