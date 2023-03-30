a06dd79c7ec28497f2ec9ef38ee90a7325dbeb6f
Jiamin-Zou <jiaminz622@gmail.com>
2023-03-30 14:09:53 -0400

    Completed two_sum

--- practice-for-ch-ruby-anagrams-long-practice-main/anagram.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagram.rb
@@ -1,7 +1,7 @@
 def anagram?(str1, str2) #O(n!)
     return false if str1.length != str2.length
     arr = str1.chars.permutation.to_a #factorial
-    arr.include?(str2.chars) #linear
+    arr.include?(str2.chars) #linear * linear
 end
 
 def sec_anagram?(str1, str2) #O(n^2)
@@ -12,7 +12,7 @@ def sec_anagram?(str1, str2) #O(n^2)
         return false if idx.nil? #constant
         str_arr.delete_at(idx) #linear
     end
-    str_arr.empty? #linear
+    str_arr.empty? #constant
 end
 
 def third_anagram?(str1, str2)
--- practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
@@ -1,13 +1,35 @@
-def two_sum?(arr, target_sum)
+def bad_two_sum?(arr, target_sum)
     (0...arr.length).each do |i| # linear
-        (i+1...arr.length).each do |j| # linear
+        (0...arr.length).each do |j|
+            next if i == j # linear
             return true if (arr[i] + arr[j]) == target_sum # constant, comparing single elements
         end
     end
     false # constant
     # O(n * n * (constant) )
+    # O(n^2)
+end
+
+def okay_two_sum?(arr, target_sum)
+    sorted_arr = arr.sort   # n log n
+    left = 0                # constant
+    right = arr.length - 1  # constant
+
+    while left < right      # linear
+        sum = sorted_arr[left] + sorted_arr[right] #constant
+        return true if sum == target_sum    # constant
+
+        if sum < target_sum #constant
+            left += 1       #constant
+        else
+            right -= 1      #constant
+        end
+    end
+    false                   #constant
+
+    # final is O(n log n)
 end
 
 arr = [0, 1, 5, 7]
-two_sum?(arr, 6) # => should be true
-two_sum?(arr, 10) # => should be false
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
